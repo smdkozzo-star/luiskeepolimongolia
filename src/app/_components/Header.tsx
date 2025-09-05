1e37b16b44fd9c59c0b8778739ce5ebb387d2b27
smdkozzo <smdkozzo@gmail.com>
2025-09-05 19:12:03 +0800

    gg

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,16 @@
 export default function Header() {
   return (
-    <div className="bg-gray-900 w-full flex justify-center items-center 
+    <div className="bg-gray-900 w-full flex justify-center gap-4 items-center 
                     h-[70px] sm:h-[80px] md:h-[90px] 
                     text-2xl sm:text-4xl md:text-6xl lg:text-7xl 
                     font-bold text-white">
+                       <img
+                               src="/logo.jpg"
+                               alt="A"
+                               width={80}
+                               height={80}
+                               className="rounded-full  max-w-full h-auto"
+                             />
       Luis Keepoli Mongolia LLC
     </div>
   );
