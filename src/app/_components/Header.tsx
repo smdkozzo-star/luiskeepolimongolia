7707235554013263c012e5fa972787b28d76a5d9
smdkozzo <smdkozzo@gmail.com>
2025-09-05 17:20:35 +0800

    done

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,9 @@
 export default function Header() {
   return (
-    <div className="bg-gray-900 h-[90px] w-full flex justify-center text-7xl items-center">
+    <div className="bg-gray-900 w-full flex justify-center items-center 
+                    h-[70px] sm:h-[80px] md:h-[90px] 
+                    text-2xl sm:text-4xl md:text-6xl lg:text-7xl 
+                    font-bold text-white">
       Luis Keepoli Mongolia LLC
     </div>
   );
